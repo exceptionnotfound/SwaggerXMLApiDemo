a1c4cbd3551450f6bee2e5505f8979c791356c80
Matthew Jones <10541872+exceptionnotfound@users.noreply.github.com>
2018-07-11 11:26:28 -0700

    ACK NOPE THIS ONE fixed the minor indent issue.

--- SwaggerXMLApiDemo/Controllers/EmployeeController.cs
+++ SwaggerXMLApiDemo/Controllers/EmployeeController.cs
@@ -7,36 +7,36 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace SwaggerXMLApiDemo.Controllers
 {
-[Route("api/[controller]")]
-[ApiController]
-public class EmployeeController : ControllerBase
-{
-    /// <summary>
-    /// Retrieve the employee by their ID.
-    /// </summary>
-    /// <param name="id">The ID of the desired Employee</param>
-    /// <returns>A string status</returns>
-    [HttpGet]
-    [Route("{id}")]
-    public ActionResult<string> GetByID(int id)
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
     {
-        return "Found Employee";
-    }
+        /// <summary>
+        /// Retrieve the employee by their ID.
+        /// </summary>
+        /// <param name="id">The ID of the desired Employee</param>
+        /// <returns>A string status</returns>
+        [HttpGet]
+        [Route("{id}")]
+        public ActionResult<string> GetByID(int id)
+        {
+            return "Found Employee";
+        }
 
-    /// <summary>
-    /// Returns a group of Employees matching the given first and last names.
-    /// </summary>
-    /// <remarks>
-    /// Here is a sample remarks placeholder.
-    /// </remarks>
-    /// <param name="firstName">The first name to search for</param>
-    /// <param name="lastName">The last name to search for</param>
-    /// <returns>A string status</returns>
-    [HttpGet]
-    [Route("byname/{firstName}/{lastName}")]
-    public ActionResult<string> GetByName(string firstName, string lastName)
-    {
-        return "Found another employee";
+        /// <summary>
+        /// Returns a group of Employees matching the given first and last names.
+        /// </summary>
+        /// <remarks>
+        /// Here is a sample remarks placeholder.
+        /// </remarks>
+        /// <param name="firstName">The first name to search for</param>
+        /// <param name="lastName">The last name to search for</param>
+        /// <returns>A string status</returns>
+        [HttpGet]
+        [Route("byname/{firstName}/{lastName}")]
+        public ActionResult<string> GetByName(string firstName, string lastName)
+        {
+            return "Found another employee";
+        }
     }
-}
 }
